3f9a12d21307138e524d866b7321536ec64237d8
SrabonyFahmida <fahmida.4323@gmail.com>
2025-07-10 02:21:21 +0000

    Introduce null safety bug in calculateTotal() method

--- Project007/Main.java
+++ Project007/Main.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
         List<MenuItem> menuItems = new ArrayList<>(
                 Arrays.asList(
--- Project007/SimpleOrderService.java
+++ Project007/SimpleOrderService.java
@@ -5,4 +5,11 @@ public class SimpleOrderService implements OrderService {
     public void placedOrder(MenuItem menuItem) {
         System.out.println("Ordering " + menuItem.getName() + " for " + menuItem.getPrice());
     }
+    public double calculateTotal(List<MenuItem> items) {
+        double total = 0;
+        for (MenuItem item : items) {
+            total += item.getPrice(); // Will crash if item is null
+        }
+        return total;
+    }
 }
